dd2e977803bf820ea854b0ea8526384b7487725b
Brooks Talley <brooks@tangentry.com>
2017-07-22 12:54:04 -0700

    More Particle porting

--- src/Adafruit_GFX.cpp
+++ src/Adafruit_GFX.cpp
@@ -806,11 +806,7 @@ void Adafruit_GFX::drawChar(int16_t x, int16_t y, unsigned char c,
     } // End classic vs custom font
 }
 
-#if ARDUINO >= 100
 size_t Adafruit_GFX::write(uint8_t c) {
-#else
-void Adafruit_GFX::write(uint8_t c) {
-#endif
     if(!gfxFont) { // 'Classic' built-in font
 
         if(c == '\n') {                        // Newline?
@@ -852,9 +848,7 @@ void Adafruit_GFX::write(uint8_t c) {
         }
 
     }
-#if ARDUINO >= 100
     return 1;
-#endif
 }
 
 void Adafruit_GFX::setCursor(int16_t x, int16_t y) {
